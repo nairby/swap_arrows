795c5a79a66f359169a1182c1000d00e3933e894
Brian Watts <brian.watts@outlook.com>
2017-07-09 17:29:46 +0100

    Update swap_arrow.cmd

--- swap_arrow.cmd
+++ swap_arrow.cmd
@@ -1,1 +1,1 @@
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t reg_binary/d 00000000000000000500000048e050e04de04be04be04de050e048e000000000 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t reg_binary /d 00000000000000000500000048e050e04de04be04be04de050e048e000000000 /f
